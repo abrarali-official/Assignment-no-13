b890e5c37d43a8c960b2f8e9cb5025a647d610f3
Abrar Ali <91776475+abrarali-official@users.noreply.github.com>
2022-02-15 13:38:12 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+    return ( <
+        div className = 'box' >
+        <
+        h1 >
+        <
+        i >
+        Title of a longer featured blog post <
+        /i> <
+        /h1> <
+        /div>
+    );
+}
+export default App;
